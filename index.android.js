f4db56342efc2d10409252c16476d2bc1d113837
FrankieLee <frankielee@seekasia.com>
2017-03-30 23:29:52 +0800

    added set default context data feature

--- index.android.js
+++ index.android.js
@@ -5,14 +5,22 @@ import ReactNative from 'react-native';
 
 var RNOmniture = ReactNative.NativeModules.omnitureImplementation
 
+const defaultContextData = {};
+
 module.exports = {
+  setDefaultContextData : (obj) => {
+    defaultContextData = Object.assign(defaultContextData, obj);
+  },
+  clearDefaultContextData : () => {
+    defaultContextData = {};
+  },
   trackAction : (str, obj)  => {
-    RNOmniture.trackAction(str, obj)
+    RNOmniture.trackAction(str, Object.assign(defaultContextData, obj))
   },
   trackState : (str, obj)  => {
-    RNOmniture.trackState(str, obj)
+    RNOmniture.trackState(str, Object.assign(defaultContextData, obj))
   },
   collectLifecycleData : (obj)  => {
-    RNOmniture.collectLifecycleData(obj)
+    RNOmniture.collectLifecycleData(Object.assign(defaultContextData, obj))
   }
 }
